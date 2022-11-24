cdcda9cb8b2102c3446a575c33d5aeb0e8ec1782
EnderixMC <enderix.mc@gmail.com>
2022-11-23 21:21:04 -0800

    Update mottos and add style.css and add banner

--- scripts/motd.js
+++ scripts/motd.js
@@ -1,7 +1,6 @@
 motds = [
     '"We make robots ...and so should you!',
     'Join us for free snacks! (Sometimes)',
-    'Robots are precise, and they are nice',
-    'Pushing with git is awesome! Sumo is good too :P'
+    'Robots are precise, but are they nice?'
 ]
 document.getElementById('motd').innerHTML = motds[Math.floor(Math.random() * motds.length)];
