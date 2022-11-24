1df3d76c0eaf373d0b161a60eb6ede0ae6a1b7b3
EnderixMC <enderix.mc@gmail.com>
2022-11-23 16:21:30 -0800

    Add mottos

--- scripts/motd.js
+++ scripts/motd.js
@@ -1,1 +1,7 @@
-document.getElementById('motd').innerHTML = '"We make robots"'
+motds = [
+    '"We make robots ...and so should you!',
+    'Join us for free snacks! (Sometimes)',
+    'Robots are precise, and they are nice',
+    'Pushing with git is awesome! Sumo is good too :P'
+]
+document.getElementById('motd').innerHTML = motds[Math.floor(Math.random() * motds.length)];
