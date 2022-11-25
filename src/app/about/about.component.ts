56c07a0a7348fc176aa946c5f8c26bf2c7416658
pythonSnake <118955679+pythonSnake5036@users.noreply.github.com>
2022-11-25 15:24:47 -0800

    Make map

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  @ViewChild("map") mapEl: any;
+  map: any;
 
+  onNgViewInit() { // @ts-ignore
+    this.map = new mapboxgl.Map({
+      container: this.mapEl.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [49.15296, -123.12896], // starting position [lng, lat]
+      zoom: 17 // starting zoom
+  });
+  }
 }
