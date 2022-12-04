93a8878045d76cb588c89f9fd1d75f123ab0b89c
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2022-12-03 18:16:36 -0800

    Delete build.sh
    This script has be replaced with github actions

--- build.sh
+++
@@ -1,9 +0,0 @@
-BRANCH=$(git branch --show-current)
-git checkout gh-pages
-git reset --hard main
-ng build --output-path docs
-cp docs/index.html docs/404.html
-git add docs/
-git commit -m "BUILD: Build files"
-git push -f
-git checkout "$BRANCH"
