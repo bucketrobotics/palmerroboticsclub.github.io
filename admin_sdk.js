1a88007e9eec3f1480e4d6de708a375f0424650c
pythonSnake <118955679+pythonSnake5036@users.noreply.github.com>
2022-12-26 21:19:06 -0800

    Update admin_sdk

--- admin_sdk.js
+++ admin_sdk.js
@@ -1,5 +1,66 @@
 var admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: applicationDefault()
+const app = admin.initializeApp();
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+
+function mainPage() {
+  rl.question('\n1. News\n2. Sponsors\n ? ', function (choice) {
+    if (choice == 1) {
+      newsPage();
+    } else if (choice == 2) {
+      console.log("Sponsor managment support to be added later");
+      mainPage();
+    } else {
+      rl.close();
+    }
+  });
+}
+
+async function newsPage() {
+  console.log("\nNews\n\nCurrent news:\n");
+  const articlesRef = admin.firestore().collection("news-articles");
+  const snapshot = await articlesRef.get();
+  snapshot.forEach(doc => {
+    console.log(doc.id);
+  });
+  rl.question('Which one do you want to inspect ? ', async function (id) {
+    const articleRef = articlesRef.doc(id);
+    const snapshot = await articleRef.get();
+    const data = snapshot.data();
+    if (articleRef.exists) {
+      articlePage(id, data);
+    } else {
+      rl.question("article doesn't exist, would you like to create a new one? [y/n] ", async function (choice) {
+        if (choice == "y") {
+          console.log("News article creation support will be added later");
+          mainPage();
+        } else {
+          newsPage();
+        }
+      });
+    }  
+  });
+}
+
+async function articlePage(id, data) {
+  console.log();
+  console.log(`${id}:`);
+  console.log(`Title: ${data["title"]}`);
+  console.log(`Type: ${data["type"]}`);
+  console.log(`Short description: ${data["short-description"]}`)
+  console.log(`Description: ${data["description"]}`);
+  rl.question("1. Edit\n2. Delete\n ? ", async function (choice) {
+
+  });
+}
+
+rl.on('close', function () {
+  process.exit(0);
+});
+
+mainPage();
