db90117733106a7b49aabef3dbb69acd5ba3be56
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-01-15 02:40:39 +0000

    Add gallery

--- src/app/team-gallery/team-gallery.component.ts
+++ src/app/team-gallery/team-gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { collection, collectionData, DocumentData, Firestore, query, orderBy } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-team-gallery',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./team-gallery.component.scss']
 })
 export class TeamGalleryComponent {
+  gallery: Observable<DocumentData[]>;
 
+  constructor(firestore: Firestore) {
+    const col = collection(firestore, 'gallery');
+    const q = query(col, orderBy("date", "desc"));
+    this.gallery = collectionData(q);
+  }
 }
