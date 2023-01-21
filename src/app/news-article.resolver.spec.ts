a95e9ef1378573bdcfbe40fd02767d6b04a77594
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-01-21 09:09:24 -0800

    Update news-article.resolver.spec.ts

--- src/app/news-article.resolver.spec.ts
+++ src/app/news-article.resolver.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { NewsArticleResolver } from './news-article.resolver';
+import { Firestore } from '@angular/fire/firestore';
+
+const FirestoreStub = {};
 
 describe('NewsArticleResolver', () => {
   let resolver: NewsArticleResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Firestore, useValue: FirestoreStub },
+      ],
+    });
     resolver = TestBed.inject(NewsArticleResolver);
   });
 
-  it('should be created', () => {
+  it('should be created', inject([Firestore], (firestore: Firestore) => {
     expect(resolver).toBeTruthy();
-  });
+  }));
 });
