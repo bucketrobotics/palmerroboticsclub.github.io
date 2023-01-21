09b4d4828d25b71768577865d84229268d03de2e
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-01-20 21:29:35 -0800

    Update get-storage-download-url.pipe.spec.ts

--- src/app/get-storage-download-url.pipe.spec.ts
+++ src/app/get-storage-download-url.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { GetStorageDownloadURLPipe } from './get-storage-download-url.pipe';
+import { BrowserModule } from '@angular/platform-browser';
+import { inject, TestBed } from '@angular/core/testing';
+import { Storage } from '@angular/fire/storage';
 
 describe('GetStorageDownloadURLPipe', () => {
   it('create an instance', () => {
     const pipe = new GetStorageDownloadURLPipe();
     expect(pipe).toBeTruthy();
   });
+});
+
+describe('GetStorageDownloadURLPipe', () => {
+  beforeEach(() => {
+    TestBed
+      .configureTestingModule({
+        imports: [
+          BrowserModule
+        ]
+      });
+  });
+
+  it('create an instance', inject([Storage], (storage: Storage) => {
+    let pipe = new GetStorageDownloadURLPipe(storage);
+    expect(pipe).toBeTruthy();
+  })); 
 });
