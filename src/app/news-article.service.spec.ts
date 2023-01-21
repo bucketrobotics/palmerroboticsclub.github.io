1a3ffa66510537bf51c174621803c17303cc23a3
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-01-21 09:07:18 -0800

    Update news-article.service.spec.ts

--- src/app/news-article.service.spec.ts
+++ src/app/news-article.service.spec.ts
@@ -2,15 +2,21 @@ import { TestBed } from '@angular/core/testing';
 
 import { NewsArticleService } from './news-article.service';
 
+const FirestoreStub = {};
+
 describe('NewsArticleService', () => {
   let service: NewsArticleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Firestore, useValue: FirestoreStub },
+      ],
+    });
     service = TestBed.inject(NewsArticleService);
   });
-
-  it('should be created', () => {
+  
+  it('should be created', inject([Firestore], (firestore: Firestore) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
