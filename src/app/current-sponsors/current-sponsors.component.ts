886c780ca2d6033d6f61aede85f59cc4e639e51a
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2023-03-08 23:56:38 +0000

    Improve sponsor loading

--- src/app/current-sponsors/current-sponsors.component.ts
+++ src/app/current-sponsors/current-sponsors.component.ts
@@ -9,29 +9,12 @@ import { Observable } from 'rxjs';
   styleUrls: ['./current-sponsors.component.scss']
 })
 export class CurrentSponsorsComponent {
-  sponsors1: Observable<DocumentData[]>;
-  sponsors2: Observable<DocumentData[]>;
-  sponsors3: Observable<DocumentData[]>;
-  sponsors4: Observable<DocumentData[]>;
-  sponsors5: Observable<DocumentData[]>;
+  sponsors: Observable<DocumentData[]>;
 
   constructor(firestore: Firestore) {
     const col = collection(firestore, 'sponsors');
 
-    const q1 = query(col, where('level', '==', 1));
-    this.sponsors1 = collectionData(q1);
-
-    const q2 = query(col, where('level', '==', 2));
-    this.sponsors2 = collectionData(q2);
-
-    const q3 = query(col, where('level', '==', 3));
-    this.sponsors3 = collectionData(q3);
-
-    const q4 = query(col, where('level', '==', 4));
-    this.sponsors4 = collectionData(q4);
-
-    const q5 = query(col, where('level', '==', 5));
-    this.sponsors5 = collectionData(q5);
+    this.sponsors = collectionData(col);
   }
 }
 
